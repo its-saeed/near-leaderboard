a0318f834db041dfb3c3ab856a37a7a6665a295f
Saeed Dadkhah <saeed@zilliqa.com>
2024-05-21 20:06:22 +0330

    Add ui

---
+++ ui/src/config.js
@@ -0,0 +1,7 @@
+const contractPerNetwork = {
+  mainnet: 'hello.near-examples.near',
+  testnet: 'highfalutin-act.testnet',
+};
+
+export const NetworkId = 'testnet';
+export const HelloNearContract = contractPerNetwork[NetworkId];
